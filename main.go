208004cda515059e6876538a59c523dbc078a9ba
ragaslan <agaslanramazan274@gmail.com>
2025-08-30 20:20:41 +0300

    feature : delete todo

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 
 	app.Get("/", getAllTodos)
 	app.Post("/", createTodo)
+	app.Delete("/:id", deleteTodo)
 
 	app.Use(func(c *fiber.Ctx) error {
 		return c.SendStatus(404)
@@ -54,5 +55,30 @@ func createTodo(ctx *fiber.Ctx) error {
 	// return saved todo as json
 
 	return ctx.Status(fiber.StatusCreated).JSON(todo)
+
+}
+
+func deleteTodo(ctx *fiber.Ctx) error {
+	// get param from url
+
+	todoId, err := ctx.ParamsInt("id")
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, " Invalid todo id "+err.Error())
+	}
+
+	var todo model.Todo
+	if err := database.DB.First(&todo, todoId).Error; err != nil {
+		return fiber.NewError(fiber.StatusNotFound, " There is no todo with this ID")
+	}
+
+	// delete record
+
+	if err := database.DB.Delete(&todo).Error; err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, " Delete error! ")
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Todo is deleted successfully",
+	})
 
 }
